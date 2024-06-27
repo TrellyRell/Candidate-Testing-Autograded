7a3d2c60f166b84ca1652e08482f5ea0b6850cf4
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-06-27 16:59:48 -0500

    part 1 finish and tested

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,12 +18,12 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let candidateName = input.quetions("Enter your name? ")
+candidateName = input.question("Enter your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+candidateAnswer = input.question(question)
 
 }
 
@@ -31,9 +31,9 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 if (candidateAnswer === correctAnswer){
-  console.log("correct answer")
+  console.log("Correct answer")
 } else{
-  console.log("incorrect answer please try again.")
+  console.log("Incorrect answer")
 }
 
 
@@ -46,7 +46,7 @@ if (candidateAnswer === correctAnswer){
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`'Welcome ${candidateName} here are a few questions.'`);
+   console.log(`Welcome ${candidateName} here are a few questions.`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
