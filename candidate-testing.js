50396db1446822381b893fce588203228e2e70a7
unknown <terrell.robinson012498@gmail.comgit config --global user.name Trellyrell>
2024-06-29 17:19:49 -0500

    part 2 done

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,39 +5,51 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = ('');
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ("Who was the first American woman in space? ");
-let correctAnswer = ("Sally Ride");
-let candidateAnswer = ("");
+let question;
+let correctAnswer;
+let candidateAnswer = [];
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = [
+  "Who was the first American woman in space? ", 
+  "True or False: 5 kilometer == 5000 meters? ",
+  "(5+3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+
+let correctAnswers = ["Sally Ride","true","40","Trajectory", "3"];
 let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("Enter your name? ")
+
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question)
-
+  let i = 0
+    while(i < questions.length) {
+      candidateAnswer[i] = input.question(questions[i]);
+      console.log(`Your answer: ${candidateAnswer[i]}`)
+      console.log(`Correct answer: ${correctAnswers[i]}`)
+      console.log("\n")
+    i++
+  }
+  
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer){
-  console.log("Correct answer")
-} else{
-  console.log("Incorrect answer")
-}
+  
+  
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade;
+  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
